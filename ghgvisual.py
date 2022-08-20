409457f368bb84f5695f762d802f146cf6feef8c
jchaghouri <josephchaghouri@sbcglobal.net>
2022-08-20 10:08:45 -0700

    fixed mistake

--- ghgvisual.py
+++ ghgvisual.py
@@ -1,6 +1,5 @@
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import numpy as np # linear algebra
-import matplotlib 
+import pandas as pd 
+import numpy as np 
 import matplotlib.pyplot as plt
 import sklearn
 import streamlit as st
